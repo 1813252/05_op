e949a7cc34dad2c522747be82fcd42a32cfc07a5
1813252 <55134827+1813252@users.noreply.github.com>
2019-09-26 16:03:58 +0900

    05

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int x,y;
+	
+	printf("input two integer : ");
+	scanf("%d %d",&x,&y);
+	
+	printf(" + result is %d",x+y);
+	
+	printf(" - result is %d",x-y);
+	
+	printf(" * result is %d",x*y);
+	
+	printf(" / result is %d",x/y);
+	
+	printf(" % result is %d",x%y);
+	
+	
+	
+	
+	
+	return 0;
+}
